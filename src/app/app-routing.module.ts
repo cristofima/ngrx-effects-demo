0b0219e23b3ea414bb8cb5083a81b8d07671e909
Cristopher Coronado Moreira <cristofima@hotmail.com>
2021-10-11 21:31:49 -0500

    App without effects

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DetailsComponent } from './pages/users/details/details.component';
+import { ListComponent } from './pages/users/list/list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: ListComponent
+  },
+  {
+    path: 'user/:id',
+    component: DetailsComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'home'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/pages/users/users.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ListComponent } from './list/list.component';
+import { DetailsComponent } from './details/details.component';
+
+@NgModule({
+  declarations: [
+    ListComponent,
+    DetailsComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    ListComponent,
+    DetailsComponent
+  ]
+})
+export class UsersModule { }
